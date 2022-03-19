e6822ee1701b8e6521ee0e9c7404f8a7eefd3010
soban <sobanshams88@gmail.com>
2022-03-19 16:56:48 +0500

    routing implementations

--- src/App.js
+++ src/App.js
@@ -2,12 +2,19 @@ import logo from './logo.svg';
 import './App.css';
 import Home from './pages/Home'
 import Faqs from './pages/Faqs'
-
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+} from "react-router-dom";
 function App() {
   return (
-    <>
-    <Home />
-    </>
+    <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/faqs" element={<Faqs />} />
+    </Routes>
+  </BrowserRouter>
   );
 }
 
