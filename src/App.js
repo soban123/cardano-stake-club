318acc223d65708cfaa14fc3f924638b052df6c7
soban <sobanshams88@gmail.com>
2022-03-19 17:29:03 +0500

    rarity added

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import './App.css';
 import Home from './pages/Home'
 import Faqs from './pages/Faqs'
 import Project from './pages/Project';
+import Rarity from './pages/Rarity';
+
 import {
   BrowserRouter,
   Routes,
@@ -14,6 +16,7 @@ function App() {
     <Routes>
       <Route path="/" element={<Home />} />
       <Route path="/project" element={<Project />} />
+      <Route path="/rarity" element={<Rarity />} />
       <Route path="/faqs" element={<Faqs />} />
     </Routes>
   </BrowserRouter>
---
+++ src/pages/Rarity.js
@@ -0,0 +1,269 @@
+import React from "react";
+
+export default function Rarity() {
+  return (
+    <div
+      data-elementor-type="wp-page"
+      data-elementor-id="4498"
+      className="elementor elementor-4498"
+      data-elementor-settings="[]"
+    >
+      <div className="elementor-section-wrap">
+        <section
+          class="elementor-section elementor-top-section elementor-element elementor-element-f7f05d1 elementor-section-content-top elementor-section-height-min-height elementor-section-items-stretch elementor-section-boxed elementor-section-height-default exad-glass-effect-no exad-sticky-section-no"
+          data-id="f7f05d1"
+          data-element_type="section"
+          data-settings='{"background_background":"slideshow","shape_divider_bottom":"triangle","shape_divider_bottom_negative":"yes","background_slideshow_gallery":[{"id":3266,"url":"https:\/\/cardanoskateclub.com\/wp-content\/uploads\/2022\/01\/SitePhoto06.jpg"},{"id":3265,"url":"https:\/\/cardanoskateclub.com\/wp-content\/uploads\/2022\/01\/SitePhoto05.jpg"},{"id":3264,"url":"https:\/\/cardanoskateclub.com\/wp-content\/uploads\/2022\/01\/SitePhoto04.jpg"},{"id":3263,"url":"https:\/\/cardanoskateclub.com\/wp-content\/uploads\/2022\/01\/SitePhoto03.jpg"},{"id":3262,"url":"https:\/\/cardanoskateclub.com\/wp-content\/uploads\/2022\/01\/SitePhoto02.jpg"},{"id":3261,"url":"https:\/\/cardanoskateclub.com\/wp-content\/uploads\/2022\/01\/SitePhoto01.jpg"}],"background_slideshow_slide_transition":"slide_left","background_slideshow_transition_duration":300,"background_slideshow_ken_burns":"yes","background_slideshow_loop":"yes","background_slideshow_slide_duration":5000,"background_slideshow_ken_burns_zoom_direction":"in"}'
+        >
+          <div class="elementor-background-overlay"></div>
+          <div
+            class="elementor-shape elementor-shape-bottom"
+            data-negative="true"
+          >
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 1000 100"
+              preserveAspectRatio="none"
+            >
+              <path
+                class="elementor-shape-fill"
+                d="M500.2,94.7L0,0v100h1000V0L500.2,94.7z"
+              />
+            </svg>{" "}
+          </div>
+          <div class="elementor-container elementor-column-gap-default">
+            <div
+              class="elementor-column elementor-col-100 elementor-top-column elementor-element elementor-element-7d90931 exad-glass-effect-no exad-sticky-section-no"
+              data-id="7d90931"
+              data-element_type="column"
+            >
+              <div class="elementor-widget-wrap elementor-element-populated">
+                <div
+                  class="elementor-element elementor-element-08b16b0 exad-sticky-section-no exad-glass-effect-no elementor-widget elementor-widget-text-editor"
+                  data-id="08b16b0"
+                  data-element_type="widget"
+                  data-settings='{"_animation":"none"}'
+                  data-widget_type="text-editor.default"
+                >
+                  <div class="elementor-widget-container">
+                    <p>this is our project rarity info</p>{" "}
+                  </div>
+                </div>
+                <div
+                  class="elementor-element elementor-element-424c0a6 animated-slow exad-sticky-section-no exad-glass-effect-no elementor-widget elementor-widget-heading"
+                  data-id="424c0a6"
+                  data-element_type="widget"
+                  data-settings='{"_animation":"none","_animation_delay":600}'
+                  data-widget_type="heading.default"
+                >
+                  <div class="elementor-widget-container">
+                    <h1 class="elementor-heading-title elementor-size-large">
+                      cardano skate club
+                    </h1>{" "}
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </section>
+        <section
+          class="elementor-section elementor-top-section elementor-element elementor-element-ba1468f elementor-hidden-mobile elementor-section-boxed elementor-section-height-default elementor-section-height-default exad-glass-effect-no exad-sticky-section-no"
+          data-id="ba1468f"
+          data-element_type="section"
+          data-settings='{"background_background":"classic","shape_divider_top":"triangle","shape_divider_bottom_negative":"yes","shape_divider_bottom":"triangle"}'
+        >
+          <div class="elementor-background-overlay"></div>
+          <div
+            class="elementor-shape elementor-shape-top"
+            data-negative="false"
+          >
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 1000 100"
+              preserveAspectRatio="none"
+            >
+              <path
+                class="elementor-shape-fill"
+                d="M500,98.9L0,6.1V0h1000v6.1L500,98.9z"
+              />
+            </svg>{" "}
+          </div>
+          <div
+            class="elementor-shape elementor-shape-bottom"
+            data-negative="true"
+          >
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 1000 100"
+              preserveAspectRatio="none"
+            >
+              <path
+                class="elementor-shape-fill"
+                d="M500.2,94.7L0,0v100h1000V0L500.2,94.7z"
+              />
+            </svg>{" "}
+          </div>
+          <div class="elementor-container elementor-column-gap-narrow">
+            <div
+              class="elementor-column elementor-col-100 elementor-top-column elementor-element elementor-element-ae5e3e7 exad-glass-effect-no exad-sticky-section-no"
+              data-id="ae5e3e7"
+              data-element_type="column"
+            >
+              <div class="elementor-widget-wrap elementor-element-populated">
+                <div
+                  class="elementor-element elementor-element-05d0c40 exad-sticky-section-no exad-glass-effect-no elementor-widget elementor-widget-heading"
+                  data-id="05d0c40"
+                  data-element_type="widget"
+                  data-widget_type="heading.default"
+                >
+                  <div class="elementor-widget-container">
+                    <h2 class="elementor-heading-title elementor-size-default">
+                      Useful tip: Press Ctrl + F and type the name of the trait
+                      you are looking for
+                    </h2>{" "}
+                  </div>
+                </div>
+                <div
+                  class="elementor-element elementor-element-818ed72 elementor-hidden-mobile exad-sticky-section-no exad-glass-effect-no elementor-widget elementor-widget-eael-adv-tabs"
+                  data-id="818ed72"
+                  data-element_type="widget"
+                  data-widget_type="eael-adv-tabs.default"
+                >
+                  <div class="elementor-widget-container">
+                    <div
+                      id="eael-advance-tabs-818ed72"
+                      class="eael-advance-tabs eael-tabs-horizontal"
+                      data-tabid="818ed72"
+                    >
+                      <div class="eael-tabs-nav">
+                        <ul class="eael-tab-top-icon">
+                          <li
+                            id="board-shape"
+                            class="inactive eael-tab-item-trigger"
+                          >
+                            <img
+                              src="https://cardanoskateclub.com/wp-content/uploads/2021/11/shape.png"
+                              alt=""
+                            />
+
+                            <span class="eael-tab-title title-after-icon">
+                              Board Shape
+                            </span>
+                          </li>
+                          <li id="board-design" class=" eael-tab-item-trigger">
+                            <img
+                              src="https://cardanoskateclub.com/wp-content/uploads/2021/12/design.png"
+                              alt=""
+                            />
+
+                            <span class="eael-tab-title title-after-icon">
+                              Board Design
+                            </span>
+                          </li>
+                          <li id="truck" class="inactive eael-tab-item-trigger">
+                            <img
+                              src="https://cardanoskateclub.com/wp-content/uploads/2021/12/truck.png"
+                              alt=""
+                            />
+
+                            <span class="eael-tab-title title-after-icon">
+                              Truck
+                            </span>
+                          </li>
+                          <li
+                            id="wheels"
+                            class="inactive eael-tab-item-trigger"
+                          >
+                            <img
+                              src="https://cardanoskateclub.com/wp-content/uploads/2021/12/wheel.png"
+                              alt=""
+                            />
+
+                            <span class="eael-tab-title title-after-icon">
+                              Wheels
+                            </span>
+                          </li>
+                          <li
+                            id="background"
+                            class="inactive eael-tab-item-trigger"
+                          >
+                            <img
+                              src="https://cardanoskateclub.com/wp-content/uploads/2021/11/background.png"
+                              alt=""
+                            />
+
+                            <span class="eael-tab-title title-after-icon">
+                              Background
+                            </span>
+                          </li>
+                          <li
+                            id="condition"
+                            class="inactive eael-tab-item-trigger"
+                          >
+                            <img
+                              src="https://cardanoskateclub.com/wp-content/uploads/2021/12/damage.png"
+                              alt=""
+                            />
+
+                            <span class="eael-tab-title title-after-icon">
+                              Condition
+                            </span>
+                          </li>
+                          <li
+                            id="specials"
+                            class="inactive eael-tab-item-trigger"
+                          >
+                            <img
+                              src="https://cardanoskateclub.com/wp-content/uploads/2022/02/rating-star-2793-copy2.png"
+                              alt=""
+                            />
+
+                            <span class="eael-tab-title title-after-icon">
+                              Specials
+                            </span>
+                          </li>
+                        </ul>
+                      </div>
+                      <div class="eael-tabs-content">
+                        <div
+                          id="board-shape-tab"
+                          class="clearfix eael-tab-content-item inactive"
+                          data-title-link="board-shape-tab"
+                        >
+                          We have decided on these three board shapes, because
+                          as we see, they best represent the skateboarding
+                          community. They represent the skaters who want to
+                          enjoy skateboarding and doing tricks, cruising around
+                          and enjoying the scenery, and last but not least
+                          something for the old school skaters.
+                          <div>
+                            <br />
+                            <table>
+                              <tbody>
+                                <tr>
+                                  <td>
+                                    Popsicle - <b>50%</b>
+                                  </td>
+                                  <td>
+                                    Cruiser - <b>30%</b>
+                                  </td>
+                                  <td>
+                                    Retro - <b>20%</b>
+                                  </td>
+                                </tr>
+                              </tbody>
+                            </table>
+                          </div>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </section>
+      </div>
+    </div>
+  );
+}
